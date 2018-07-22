9e8df3d18af3178e38e18548174e5b99423a322f
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 20:47:55 +0300

    test

--- validate_packer.sh
+++ validate_packer.sh
@@ -3,12 +3,15 @@
 # Description: Validate packer templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
+# Show used checker versions
+packer version
+
 maindir=$(pwd)
 
-cd packer
+cd packer || exit
 packer validate -var-file=variables.json.example ubuntu16.json
 packer validate -var-file=variables.json.example immutable.json
 
-cd $maindir
+cd "$maindir" || exit
 packer validate -var-file=packer/variables.json.example packer/app.json
 packer validate -var-file=packer/variables.json.example packer/db.json
--- validate_terraform.sh
+++ validate_terraform.sh
@@ -3,19 +3,24 @@
 # Description: Validate terraform templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
+# Show used checker versions
+terraform version; tflint --version
+
 # Generate dummy ssh-key for terraform
+
+
 mkdir ~/.ssh
 ssh-keygen -C appuser -t rsa -f ~/.ssh/appuser -P dummy
 
 cd terraform || exit
 tfdir=$(pwd)
-echo $tfdir
 
-cd $tfdir/stage/ || exit
+cd "$tfdir"/stage/ || exit
 terraform init -backend=false
 terraform validate -var-file=terraform.tfvars.example
+tflint --error-with-issues -var-file=terraform.tfvars.example
 
-cd $tfdir/prod/ || exit
+cd "$tfdir"/prod/ || exit
 terraform init -backend=false
 terraform validate -var-file=terraform.tfvars.example
-
+tflint --error-with-issues -var-file=terraform.tfvars.example
