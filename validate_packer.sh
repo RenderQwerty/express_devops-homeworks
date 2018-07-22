52394a702ece9e15b9e75bcfa0cc1d4c8b408a72
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 20:24:04 +0300

    a

--- validate_packer.sh
+++ validate_packer.sh
@@ -3,9 +3,12 @@
 # Description: Validate packer templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
+maindir=$(pwd)
 
-ls -l
-packer validate -var-file=packer/variables.json.example packer/ubuntu16.json
-packer validate -var-file=packer/variables.json.example packer/immutable.json
+cd packer
+packer validate -var-file=variables.json.example ubuntu16.json
+packer validate -var-file=variables.json.example immutable.json
+
+cd $maindir
 packer validate -var-file=packer/variables.json.example packer/app.json
 packer validate -var-file=packer/variables.json.example packer/db.json
