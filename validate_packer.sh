a59c1de9c7f8a0adaca24b4de030030568631209
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 19:54:38 +0300

    script fix

--- validate_packer.sh
+++ validate_packer.sh
@@ -3,6 +3,6 @@
 # Description: Validate packer templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
-cd packer
+cd packer || exit
 ls -l
 packer validate -var-file=variables.json.example ubuntu16.json
