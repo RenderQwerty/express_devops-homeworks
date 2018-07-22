019800be18e9203059e279bca47119d8ea3440b9
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 20:31:59 +0300

    tf

--- validate_terraform.sh
+++ validate_terraform.sh
@@ -3,6 +3,10 @@
 # Description: Validate terraform templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
+# Generate dummy ssh-key for terraform
+mkdir ~/.ssh
+ssh-keygen -C appuser -t rsa -f ~/.ssh/appuser -P dummy
+
 cd terraform || exit
 tfdir=$(pwd)
 echo $tfdir
@@ -11,7 +15,7 @@ cd $tfdir/stage/ || exit
 terraform init -backend=false
 terraform validate -var-file=terraform.tfvars.example
 
-cd $tfdir/prod || exit
+cd $tfdir/prod/ || exit
 terraform init -backend=false
 terraform validate -var-file=terraform.tfvars.example
 
