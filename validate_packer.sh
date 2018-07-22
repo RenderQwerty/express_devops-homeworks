74117cbcf3a32151051359c8886499e98a9967b5
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 20:00:43 +0300

    script fix

--- validate_packer.sh
+++ validate_packer.sh
@@ -3,6 +3,9 @@
 # Description: Validate packer templates
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
-cd packer || exit
+
 ls -l
-packer validate -var-file=variables.json.example ubuntu16.json
+packer validate -var-file=packer/variables.json.example packer/ubuntu16.json
+packer validate -var-file=packer/variables.json.example packer/immutable.json
+packer validate -var-file=packer/variables.json.example packer/app.json
+packer validate -var-file=packer/variables.json.example packer/db.json
