baa3649d422e34686ab97bf835e02b18c72999b4
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-22 21:03:36 +0300

    linter added

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,7 +3,7 @@
 # Description: Install all dependecies for travis-ci tests
 # Maintainter: Yurii Fisakov, fisakov.root@gmail.com
 
-deps=( unzip )
+deps=( unzip python-pip )
 packer_url=https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
 terraform_url=https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 tflint_url=https://github.com/wata727/tflint/releases/download/v0.7.1/tflint_linux_amd64.zip
@@ -14,6 +14,7 @@ do
 done 
 
 
+pip install ansible-lint
 wget $packer_url -O /tmp/packer.zip; unzip /tmp/packer -d /usr/sbin/
 wget $terraform_url -O /tmp/terraform.zip; unzip /tmp/terraform.zip -d /usr/sbin/
 wget $tflint_url -O /tmp/tflint.zip; unzip /tmp/tflint.zip -d /usr/sbin/
---
+++ validate_ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Description: Validate terraform templates
+# Maintainter: Yurii Fisakov, fisakov.root@gmail.com
+
+# Show used checker versions
+anible-lint --version
